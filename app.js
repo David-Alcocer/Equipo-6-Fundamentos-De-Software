6c6d14bcc9a8684228f7f9f18133adfe125f803c
MisaelDev <misaelalcocer1906@gmail.com>
2025-10-09 20:07:19 -0600

    test

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
+const loginForm = document.getElementById("loginForm");
+const registerForm = document.getElementById("registerForm");
+const showRegister = document.getElementById("showRegister");
+const showLogin = document.getElementById("showLogin");
+
+showRegister.addEventListener("click", (e) => {
+  e.preventDefault();
+  loginForm.classList.add("hidden");
+  registerForm.classList.remove("hidden");
+});
+
+showLogin.addEventListener("click", (e) => {
+  e.preventDefault();
+  registerForm.classList.add("hidden");
+  loginForm.classList.remove("hidden");
+});
+
 loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const email = document.getElementById("loginEmail").value.trim();
   const password = document.getElementById("loginPassword").value.trim();
 
-  if (email === "" || password === "") {
-    alert("Por favor, llena todos los campos.");
+  if (!email || !password) {
+    alert("Por favor completa todos los campos.");
     return;
   }
 
-  // Simulación de validación básica (sin backend)
   if (email.endsWith("@alumnos.uady.mx")) {
     alert("Inicio de sesión exitoso ✅");
     window.location.href = "dashboard.html";
   } else {
-    alert("Solo se permite el correo institucional (@alumnos.uady.mx)");
+    alert("Solo se permiten correos institucionales (@alumnos.uady.mx)");
   }
 });
